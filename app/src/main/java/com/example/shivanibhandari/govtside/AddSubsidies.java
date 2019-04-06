c3812343a1be957bf1a5f62835ee052483e3438d
Shivani Bhandari <bhandari.shivani16@gmail.com>
2019-04-06 16:52:17 +0530

    adding changes

--- app/src/main/java/com/example/shivanibhandari/govtside/AddSubsidies.java
+++ app/src/main/java/com/example/shivanibhandari/govtside/AddSubsidies.java
@@ -2,12 +2,33 @@ package com.example.shivanibhandari.govtside;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddSubsidies extends AppCompatActivity {
+    private EditText eSubsidyName;
+    private EditText eAmountPerHectare;
+    private Spinner sSubsidyType;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_subsidies);
+
+        eSubsidyName = findViewById(R.id.eSubsidyName);
+        eAmountPerHectare = findViewById(R.id.eAmountPerHectare);
+        sSubsidyType = findViewById(R.id.sSubsidyType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Subsidy_Type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sSubsidyType.setAdapter(adapter);
+
+
+
+
+
     }
 }
